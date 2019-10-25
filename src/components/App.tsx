b4d0b7f4de4d3a1499ae765207567ade038014be
Tim Smeets <tla.smeets@gmail.com>
2019-10-25 22:52:09 +0200

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,7 +9,7 @@ class App extends React.Component {
           <h1 className="App-title">how long</h1>
         </header>
         <p className="App-intro">
-          Testing out TeamCity TestDrive triggers.
+          Testing out TeamCity TestDrive triggers.... please build
         </p>
       </div>
     );
